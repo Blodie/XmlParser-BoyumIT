3b9bbe7c78e35069c0edcff4628bca85242c6caf
Benjamin Kovacs <benjamin.kovacs@capgemini.com>
2024-03-25 22:28:38 +0100

    Add WebOrder model.

---
+++ XmlParser.Main/Models/WebOrder.cs
@@ -0,0 +1,26 @@
+﻿using System.Xml.Serialization;
+
+namespace XmlParser.Main.Models;
+
+[XmlRoot("WebOrder")]
+public class WebOrder
+{
+	[XmlAttribute("id")]
+	public int Id { get; set; }
+
+	[XmlElement("Customer")]
+	public string? Customer { get; set; }
+
+	[XmlIgnore]
+    public DateOnly Date
+	{
+		get => DateOnly.TryParseExact(DateString, "yyyyMMdd", out var date) ? date : default; 
+		set => DateString = value.ToString("yyyyMMdd"); 
+	}
+
+	[XmlElement("Date")]
+	public string? DateString { get; set; }
+
+	[XmlArray("Items")]
+	public List<WebOrderItem>? Items { get; set; }
+}
---
+++ XmlParser.Main/Models/WebOrderItem.cs
@@ -0,0 +1,19 @@
+﻿using System.Xml.Serialization;
+
+namespace XmlParser.Main.Models;
+
+[XmlRoot("Item")]
+public class WebOrderItem
+{
+	[XmlAttribute("id")]
+	public string Id { get; set; } = default!;
+
+	[XmlAttribute("description")]
+	public string? Description { get; set; }
+
+	[XmlElement("Quantity")]
+	public int Quantity { get; set; }
+
+	[XmlElement("Price")]
+	public decimal Price { get; set; }
+}
