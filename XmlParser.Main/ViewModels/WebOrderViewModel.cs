76a8e116d02259086bc6ef8e32c36b16c76dd61a
Benjamin Kovacs <benjamin.kovacs@capgemini.com>
2024-03-26 02:07:35 +0100

    Add ViewModel.

---
+++ XmlParser.Main/ViewModels/WebOrderViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace XmlParser.Main.ViewModels;
+
+public class WebOrderViewModel
+{
+    public int Id { get; set; }
+    public string? Customer { get; set; }
+
+	[DisplayFormat(DataFormatString = "{0:dd. MMMM. yyyy}")]
+    public DateOnly Date { get; set; }
+
+    [DisplayFormat(DataFormatString = "{0:#,###.000}")]
+    public decimal Total { get; set; }
+
+    [DisplayFormat(DataFormatString = "{0:#,###.000}")]
+    public decimal Average { get; set; }
+}
