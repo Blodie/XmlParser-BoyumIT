971a8c1753b8c1077728962ec93ee407a7c98c15
Benjamin Kovacs <benjamin.kovacs@capgemini.com>
2024-03-26 22:54:26 +0100

    Add unit tests for WebOrderMapper.

---
+++ XmlParser.Tests.Unit/Helpers.cs
@@ -0,0 +1,21 @@
+﻿using XmlParser.Main.Models;
+
+namespace XmlParser.Tests.Unit;
+public static class Helpers
+{
+    public static WebOrder GetValidWebOrderModel()
+    {
+        return new()
+        {
+            Id = 42,
+            Customer = "John Smith",
+            Date = "20160327",
+            Items = new()
+            {
+                new() { Id = "B1234", Description = "SAP Business One", Quantity = 4, Price = 133.33m },
+                new() { Id = "A5678", Description = "Microsoft Office 2016", Quantity = 2, Price = 1143.00m },
+                new() { Id = "A2222", Description = "MSSQL Server 2014", Quantity = 2, Price = 110.30m },
+            }
+        };
+    }
+}
---
+++ XmlParser.Tests.Unit/MapperTests/WebOrderMapperTests.cs
@@ -0,0 +1,56 @@
+﻿using FluentAssertions;
+
+using XmlParser.Main.Mappers;
+
+namespace XmlParser.Tests.Unit.MapperTests;
+public class WebOrderMapperTests
+{
+    private readonly WebOrderMapper _sut = new();
+
+    [Fact]
+    public void MapToViewModel_ShouldMapWebOrderToWebOrderViewModel_WhenModelPropertiesAreNotNullAndValid()
+    {
+        var model = Helpers.GetValidWebOrderModel();
+
+        var result = _sut.MapToViewModel(model);
+
+        result.Id.Should().Be(42);
+        result.Customer.Should().Be("John Smith");
+        result.Date.Should().Be(new DateOnly(2016, 3, 27));
+        result.Average.Should().Be(379.99m);
+        result.Total.Should().Be(3039.92m);
+    }
+
+    [Fact]
+    public void MapToViewModel_ShouldThrowDivideByZeroException_WhenModelItemsQuantitiesSumIsZero()
+    {
+        var model = Helpers.GetValidWebOrderModel();
+        model.Items!.ForEach(item => item.Quantity = 0);
+
+        var result = () => _sut.MapToViewModel(model);
+
+        result.Should().Throw<DivideByZeroException>();
+    }
+
+    [Fact]
+    public void MapToViewModel_ShouldThrowDivideByZeroException_WhenModelItemsCountIsZero()
+    {
+        var model = Helpers.GetValidWebOrderModel();
+        model.Items = new();
+
+        var result = () => _sut.MapToViewModel(model);
+
+        result.Should().Throw<DivideByZeroException>();
+    }
+
+    [Fact]
+    public void MapToViewModel_ShouldThrowException_WhenItemsIsNull()
+    {
+        var model = Helpers.GetValidWebOrderModel();
+        model.Items = null;
+
+        var result = () => _sut.MapToViewModel(model);
+
+        result.Should().Throw<ArgumentNullException>();
+    }
+}
